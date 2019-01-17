e003c444c4a56c42926eaa07234507fdee746243
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-17 20:46:43 +0100

    Upload of 00_Obscur_Trader.rb and related spec file

---
+++ lib/00_obscur_trader.rb
@@ -0,0 +1,40 @@
+#! /usr/bin/env ruby
+##****************************************************************************
+# RUBY - Obscur Trader
+#****************************************************************************
+#   Ruby's Program - obscur_trader
+#   Written by: Guillaume CHRISTE
+# 	Date: 17-Jan-
+#   
+#   Description:
+# 		- From an HTML Page we recover the name of the Cryptomoney Symbol & it's price
+# 		- Each item is stored in a dedicated array then merged (zip) in a hash
+# 		 
+# 	Gems:
+		require 'rubygems'
+		require 'rubocop'
+		require 'nokogiri'
+		require 'open-uri'
+#
+#****************************************************************************
+def obscur_trader
+  begin
+    url = "https://coinmarketcap.com/all/views/all/"
+    html = Nokogiri::HTML(open(url))
+
+    tblSymbol =[]
+    tblPrice =[]
+
+    html.css('td.text-left.col-symbol').map {|symbol| tblSymbol << symbol.text}
+    html.css('.price').map {|price| tblPrice << (price.text)[1..-1]} #To remove the $ symbole
+
+    hash = tblSymbol.zip(tblPrice)
+
+  rescue StandardError => e
+  	puts "The url #{url} is not valid and/or structure of the table has changed"
+  end
+end
+# End of obscur_trader .......................................................
+#.............................................................................
+#.............................................................................
+obscur_trader
---
+++ spec/00_obscur_trader_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/00_obscur_trader'
+
+describe "return table" do
+  it "the type returned is an array" do
+    expect(obscur_trader.class).to eq(Array)
+  end
+  it "the returns something" do
+    expect(obscur_trader).not_to be_nil
+  end
+end
